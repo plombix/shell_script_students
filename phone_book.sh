62fb513a7363273cdd6ac311eabafae190d54831
plombix <plombix@tour>
2016-10-25 19:52:36 +0200

    Add file redirection

--- phone_book.sh
+++ phone_book.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 num_of_files=$( ls -l people | wc -l )
-
+touch phone_book.txt
+echo "">phone_book.txt
 for (( c=0; c<=$num_of_files; c++ ))
 do
   ls people -1 | while read line;
@@ -10,25 +11,25 @@ do
         then
          if  [[ $line =~ \.name$ ]]
            then
-           echo "${line} is a the card's nb ${c} name :"
-           cat "people/${line}"
+           echo "$line is a the card's nb ${c} name :"
+           cat "people/$line" >>phone_book.txt
          elif  [[ $line =~ \.png$ ]]
            then
-           echo "${line} is a the card's nb ${c} pict"
+           echo "$line is a the card's nb ${c} pict"
          elif  [[ $line =~ \.tel$ ]]
            then
-           echo "${line} is a the card's nb ${c} phone data :"
-            cat "people/${line}"
+           echo "$line is a the card's nb ${c} phone data :"
+            cat "people/$line">>phone_book.txt
          elif [[ $line =~ \.work$ ]]
            then
-           echo "${line} is a the card's nb ${c} job description :"
-            cat "people/${line}"
+           echo "$line is a the card's nb ${c} job description :"
+            cat "people/$line">>phone_book.txt
          elif  [[ $line =~ \.add$ ]]
            then
-           echo "${line} is a the card's nb ${c} address :"
-            cat "people/${line}"
+           echo "$line is a the card's nb ${c} address :"
+            cat "people/$line">>phone_book.txt
          else
-           echo "${line} is something i dont know "
+           echo "$line is something i dont know "
          fi
        fi
     done
