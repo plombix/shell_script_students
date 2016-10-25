a2f6562dade4ebf5db17af684ac8e4e093f1a2e3
plombix <plombix@tour>
2016-10-25 16:11:58 +0200

    Add regexp to filter image

--- phone_book.sh
+++ phone_book.sh
@@ -3,4 +3,8 @@
 ls people -1 | while read line;
   do
       echo "One file is named  ${line}"
+      if [[ $line =~ \.png$ ]]
+        then
+         echo "${line} is a pict file "
+       fi
   done
