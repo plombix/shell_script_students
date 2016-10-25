507ffa377db7554ee0dc715b2ef9406df64b3e7f
plombix <plombix@tour>
2016-10-25 18:45:12 +0200

    Add double loop

--- phone_book.sh
+++ phone_book.sh
@@ -1,23 +1,28 @@
 #!/bin/bash
 
-ls people -1 | while read line;
-  do
-        if [[ $line =~ \.png$ ]]
-         then
-          echo "${line} is a pict file "
-        elif [[ $line =~ \.tel$ ]]
-         then
-          echo "${line} is a phone file "
-        elif [[ $line =~ \.name$ ]]
-         then
-          echo "${line} is a name file "
-        elif [[ $line =~ \.work$ ]]
-         then
-          echo "${line} is a job desc file "
-        elif [[ $line =~ \.add$ ]]
-         then
-          echo "${line} is a address file "
-        else
-          echo "${line} is something i dont know "
-        fi
-  done
+num_of_files=$( ls -l people | wc -l )
+
+for (( c=0; c<=$num_of_files; c++ ))
+do
+  ls people -1 | while read line;
+    do
+      if [[ $line = $c* ]]
+        then
+         if  [[ $line =~ \.png$ ]]
+           then
+           echo "${line} is a the card's nb ${c} pict"
+         elif  [[ $line =~ \.tel$ ]]
+           then
+           echo "${line} is a the card's nb ${c} phone data "
+         elif [[ $line =~ \.work$ ]]
+           then
+           echo "${line} is a the card's nb ${c} job description"
+         elif  [[ $line =~ \.add$ ]]
+           then
+           echo "${line} is a the card's nb ${c} address"
+         else
+           echo "${line} is something i dont know "
+         fi
+       fi
+    done
+done
