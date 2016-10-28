24245853fb2dbed359e7f26deae225fbb2168785
Raph Nis <raphaellenis@gmail.com>
2016-10-28 16:30:50 +0200

    ajout de l'explication de chaque terme devant ex nom:

--- phone_book.sh
+++ phone_book.sh
@@ -12,7 +12,7 @@ echo "<!DOCTYPE html>
 <body>" > people/phone_book.html
 for (( c=0; c<=$num_of_files; c++ ))
 do
-  ls people -1 | while read line;
+  ls people | while read line;
     do
       if [[ $line = $c* ]]
         then
@@ -20,14 +20,14 @@ do
            then
            echo "$line is a the card's nb $c name :"
            name=$(cat "people/$line")
-           echo "<p class='fn'><a class='url' href='http://www.google.com/search?q=${name}'>${name}</a><p>">> people/phone_book.html
+           echo "<p class='fn'>Nom, prenom: <a class='url' href='http://www.google.com/search?q=${name}'>${name}</a><p>">> people/phone_book.html
          elif [[ $line =~ \.work$ ]]
            then
            echo "$line is a the card's nb $c job description :"
             cat "people/$line"
             job=$(cat "people/$line")
             wikijob=$(cat "people/$line" | sed 's/ /+/g')
-            echo "<p class='fn'><a class='url' href='https://fr.wikipedia.org/w/index.php?search=${wikijob}'>${job}</a><p>">> people/phone_book.html
+            echo "<p class='fn'>Métier: <a class='url' href='https://fr.wikipedia.org/w/index.php?search=${wikijob}'>${job}</a><p>">> people/phone_book.html
          elif  [[ $line =~ \.png$ ]]
            then
            echo "$line is a the card's nb $c pict"
@@ -37,13 +37,13 @@ do
            echo "$line is a the card's nb $c address :"
             cat "people/$line"
             add=$(cat "people/$line")
-            echo "<p class='adr'>${add}</p>">> people/phone_book.html
+            echo "<p class='adr'>Adresse: ${add}</p>">> people/phone_book.html
          elif  [[ $line =~ \.tel$ ]]
            then
            echo "$line is a the card's nb $c phone data :"
             cat "people/$line"
             tel=$(cat -e "people/$line" | sed  's/\$/<br>/g')
-            echo "<p class='tel'>${tel}</p>" >> people/phone_book.html
+            echo "<p class='tel'>Numéro de téléphone: ${tel}</p>" >> people/phone_book.html
          else
            echo "$line is something i dont know "
          fi
