997fd83e5f6c1dbf86354c88c3642dfe77f6182d
Flrnhd2 <florian.hernandez07@gmail.com>
2016-11-01 20:45:08 +0100

    Add variable file and comments to phone_book.sh

--- phone_book.sh
+++ phone_book.sh
@@ -1,59 +1,65 @@
 #!/bin/bash
 
+# Déclaration de variables préalables
 num_of_files=$( ls -l people | wc -l )
-touch "people/phone_book.html"
+file='phone_book.html' # Enregistrement du chemin du fichier dans une variable pour ne pas réécrire le chemin à chaque fois
+
+# Création du fichier .html
+touch "phone_book.html"
+
+# Enregistrement des premieres lignes de la structure de la page html
 echo "<!DOCTYPE html>
 <html>
 <head>
  <meta charset='UTF-8'>
  <title>Phone_book</title>
 </head>
+<body>" > $file
 
-<body>" > people/phone_book.html
+# Traitement des fichiers
 for (( c=0; c<=$num_of_files; c++ ))
 do
   ls people | while read line;
-    do
-      if [[ $line = $c* ]]
-        then
-        case $line in
-          *name)
-           echo "$line is a the card's nb $c name :"
-           name=$(cat "people/$line")
-           echo "<p class='fn'>Nom, prenom: <a class='url' href='http://www.google.com/search?q=${name}'>${name}</a><p>">> people/phone_book.html
-           ;;
-          *work)
-           echo "$line is a the card's nb $c job description :"
-            cat "people/$line"
-            job=$(cat "people/$line")
-            wikijob=$(cat "people/$line" | sed 's/ /+/g')
-            echo "<p class='fn'>Métier: <a class='url' href='https://fr.wikipedia.org/w/index.php?search=${wikijob}'>${job}</a><p>">> people/phone_book.html
-            ;;
-          *png)
-            echo "$line is a the card's nb $c pict"
-            echo "<img src='$line' alt='${name}' height='100' width='100'>">> people/phone_book.html
-            ;;
-         *add)
-           echo "$line is a the card's nb $c address :"
-            cat "people/$line"
-            add=$(cat "people/$line")
-            echo "<p class='adr'>Adresse: ${add}</p>">> people/phone_book.html
-            ;;
-         *tel)
-           echo "$line is a the card's nb $c phone data :"
-            cat "people/$line"
-            tel=$(cat -e "people/$line" | sed  's/\$/<br>/g')
-            echo "<p class='tel'>Numéro de téléphone: ${tel}</p>" >> people/phone_book.html
-            ;;
-          *)
-           echo "$line is something i dont know "
-           ;;
-         esac
-       fi
+  do
+    if [[ $line = $c* ]]
+      then
+      case $line in
+        *name)
+          echo "$line is the card's nb $c name :"
+          name=$(cat "people/$line")
+          echo "<p class='fn'>Nom, prenom: <a class='url' href='http://www.google.com/search?q=${name}'>${name}</a><p>">> $file
+          ;;
+        *work)
+          echo "$line is the card's nb $c job description :"
+          cat "people/$line"
+          job=$(cat "people/$line")
+          wikijob=$(cat "people/$line" | sed 's/ /+/g')
+          echo "<p class='fn'>Métier: <a class='url' href='https://fr.wikipedia.org/w/index.php?search=${wikijob}'>${job}</a><p>">> $file
+          ;;
+        *png)
+          echo "$line is the card's nb $c pict"
+          echo "<img src='$line' alt='${name}' height='100' width='100'>">> $file
+          ;;
+        *add)
+          echo "$line is the card's nb $c address :"
+          cat "people/$line"
+          add=$(cat "people/$line")
+          echo "<p class='adr'>Adresse: ${add}</p>">> $file
+          ;;
+        *tel)
+          echo "$line is the card's nb $c phone data :"
+          cat "people/$line"
+          tel=$(cat -e "people/$line" | sed  's/\$/<br>/g')
+          echo "<p class='tel'>Numéro de téléphone: ${tel}</p>" >> $file
+          ;;
+        *)
+          echo "$line is something i dont know..."
+          ;;
+        esac
+      fi
     done
-    echo "<br />" >>people/phone_book.html
-    echo "<br />" >>people/phone_book.html
+    echo "<br /><hr />" >> $file
 done
 echo "
 </body>
-</html>" >> people/phone_book.html
+</html>" >> $file
